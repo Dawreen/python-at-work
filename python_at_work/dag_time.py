273d7e64a9ffed0b6a9f094790bba0b9de9fb79e
Dan <dntknwhtf@gmail.com>
2024-04-12 17:22:15 +0200

    dag time

--- python_at_work/dag_time.py
+++ python_at_work/dag_time.py
@@ -1,7 +1,19 @@
 import logging
+import json
+import yaml
 
 
 LOGGER = logging.getLogger(__name__)
+
+def get_time(dag_id: str) -> str:
+    f = open('config.json')
+    data = json.load(f)
+    f.close()
+
+    path_dag = f"{data["path"]}/{dag_id}/config/dag_config.yaml" 
+    with open(path_dag, 'r') as file:
+        dag_config = yaml.safe_load(file)
+        return dag_config["dag_parameters"]["schedule"]
 
 def get_DAG(table_id: str) -> str:
     return ""
--- tests/test_dag_time.py
+++ tests/test_dag_time.py
@@ -3,7 +3,7 @@ import json
 import os
 import logging
 
-from python_at_work.dag_time import get_DAG
+from python_at_work.dag_time import get_DAG, get_time
 
 
 LOGGER = logging.getLogger(__name__)
@@ -12,9 +12,11 @@ def test_print_word():
     f = open('config.json')
     data = json.load(f)
     f.close()
-    
+
     path_dags = data["path"]
     my_dag = get_DAG("project.dataset.table")
-    LOGGER.info(str(os.listdir(path_dags)))
+    dag_check = "account_enriched"
+    time = get_time(dag_check)
+    LOGGER.info(f"DAG:{dag_check} --- {time}")
 
-    assert True
+    assert time == "35 00 * * *"
