2c2ab9819f28f4a0fdb24bea8785829b1c94a296
Dan <dntknwhtf@gmail.com>
2024-04-12 16:01:47 +0200

    update on code

--- python_at_work/query_dependencies.py
+++ python_at_work/query_dependencies.py
@@ -1,25 +1,17 @@
 import logging
+import re
 
 
 LOGGER = logging.getLogger(__name__)
 
 def dependency_list(path: str) -> list[str]:
-    LOGGER.info('Searching')
     query_rows = []
-    dep_list = []
+    res = []
     with open(path, "r+") as query:
         # Reading from a file
         query_rows = query.read().split("\n")
-        LOGGER.info(f'query has {len(query_rows)} rows')
-    if len(query_rows) == 0:
-        LOGGER.warning("Query has no rows")
     for row in query_rows:
-        row = row.replace(" ", "").lower()
-        is_from = row.split("from")
-        if len(is_from) > 1:
-            for part in is_from:
-                if len(part.split(".")) > 2:
-                    LOGGER.info(str(part))
-                    dep_list.append(part)
-    
-    return dep_list
+        filtered = re.findall(r'\`([^`]*)\`', row)
+        if filtered:
+            res.extend(filtered)
+    return res
--- tests/test_query_dependencies.py
+++ tests/test_query_dependencies.py
@@ -1,9 +1,15 @@
 import pytest
 import sys
+import logging
+
 from python_at_work.query_dependencies import dependency_list
+
+
+LOGGER = logging.getLogger(__name__)
 
 def test_print_word():
     dep_list = dependency_list("query.sql")
-    res = ["`$source_project.operational_support.wifi_test_trigger`",
-           "`$source_project.operational_support.wifi_test`"]
+    res = ["$source_project.operational_support.wifi_test_trigger",
+           "$source_project.operational_support.wifi_test"]
+    LOGGER.info(str(dep_list))
     assert dep_list == res
