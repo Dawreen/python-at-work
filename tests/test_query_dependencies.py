e76ff6964e4f7c76e3c804273caed9e56584c972
Dan <dntknwhtf@gmail.com>
2024-04-12 12:13:20 +0200

    fix on test

--- tests/test_query_dependencies.py
+++ tests/test_query_dependencies.py
@@ -4,6 +4,6 @@ from python_at_work.query_dependencies import dependency_list
 
 def test_print_word():
     dep_list = dependency_list("query.sql")
-    res = ["`$source_project_daita.operational_support.skywifi_test_trigger`",
-           "`$source_project_daita.operational_support.skywifi_test`"]
+    res = ["`$source_project.operational_support.wifi_test_trigger`",
+           "`$source_project.operational_support.wifi_test`"]
     assert dep_list == res
